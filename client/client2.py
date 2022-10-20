ee45439e6264a6d773c0b9146eb2ec4f5844fe7e
eduardoferreira97 <animedudu12345@gmail.com>
2022-10-19 21:44:16 -0300

    Code comment

--- client/client2.py
+++ client/client2.py
@@ -5,9 +5,11 @@ import os
 class Cliente:
 
     def __init__(self):
+        # Cria o socket
         self.s = socket.socket()         
         self.host = socket.gethostname() 
-        self.port = 49157                
+        self.port = 49158
+        # Associa um IP e Porta para o servidor 
         self.s.bind((self.host, self.port))   
 
         try:
@@ -18,26 +20,29 @@ class Cliente:
             print("Tem alguma coisa errada com %s:%d. O erro é %s" % (self.host, 49159, e))
             
     def rodando(self):
-# while True:
+        # O usuário da o input para a ação requerida
         Answer = input("download/ upload/ exit: ")
 
         if(Answer == "download"):
-
+            # Envia para o servidor a ação que será feita
             mssg = "download"
             self.s.send(mssg.encode())
+
+            # Lista todos os arquivos encontrados no diretório do servidor
             print("Arquivos:")
             for arq in os.listdir("/home/eduardo/Documentos/Projetos/Redes 1/Projeto 2/server"):
                 print(arq)
             print("\n")
             FileName = input("Digite o nome do arquivo que vai ser baixado: ")
-            Data = "Temp"
+            # Data = "Temp"
 
             while True:
-
+                # Envia para o servidor o nome do arquivo solicitado
                 self.s.send(FileName.encode())
                 Data = self.s.recv(1024)
+                # Abrindo o arquivo que vai ser baixado
                 DownloadFile = open(FileName,"wb")
-
+                # Loop para "escrever" o arquivo selecionado
                 while Data:
 
                     DownloadFile.write(Data)
@@ -48,24 +53,26 @@ class Cliente:
                 break
                 
         elif(Answer == "upload"):
-
+            # Envia para o servidor a ação que será feita
             mssg = "upload"
             self.s.send(mssg.encode())
-
+            # Lista todos os arquivos encontrados no diretório do servidor
             print("Arquivos:")
             for arqs in os.listdir(os.getcwd()+"/files"):
                 print(arqs)
             print("\n")
             
             FileName = input("Coloque o nome o arquivo que vai ser eniado: ")
+            # Envia para o servidor o nome do arquivo
             self.s.send(FileName.encode())
-
+            # Abri o arquivo em binário para ser lido
             UploadFile = open("/home/eduardo/Documentos/Projetos/Redes 1/Projeto 2/client/files//"+FileName,"rb")
             Read = UploadFile.read(1024)
-                
+            # Envia o arquivo lido para o servidor    
             while Read:
                 self.s.send(Read) #Envia 1KB 
                 Read = UploadFile.read(1024)
+                
             print("Arquivo enviado")
             UploadFile.close()
 
--- server/server.py
+++ server/server.py
@@ -4,57 +4,79 @@ import os
 
 #49152-65535
 class ThreadedServer():
+
     def __init__(self):
+        
         self.host = socket.gethostname()
         self.port = 49159
+        # Cria o socket
+        # F_INET (família de endereço IPV4) e SOCK_STREAM (TCP)
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.s.bind((self.host, self.port))                          
                                                                      
-        
 
     def listen(self):
+        # Habilita que o servidor aceite conexões
         self.s.listen(5)
         while True:
+            # Aceit a conexão do cliente no servidor
             c, addr = self.s.accept()
-            # c.settimeout(0)
+            # Permite que mais de um cliente utilizem o servidor
             threading.Thread(target = self.listenToClient,args = (c,addr)).start()
 
     def listenToClient(self, c, addr):
 
         print('Conexão estabelecida', addr)
 
+        
+        # Recebe a ação do cliente
         data = c.recv(1024)
-    
+
+
         if (data.decode() == "download"):
+            # Recebe o nome do arquivo
             FileName = c.recv(1024)
+
+            # Percorrer o diretório
             for file in os.listdir("/home/eduardo/Documentos/Projetos/Redes 1/Projeto 2/server"):
+
                 if file == FileName.decode():
                     FileFound = 1
                     break
 
             if FileFound == 0:
+                # Caso o arquivo não exista, ele printaque o arquivo não existe.
                 c.send()
                 print("Não encontrado no servidor")
 
             else:
-                print("Arquivo baixado")
+                # Caso o arquivo exista, ele vai decodificar as suas informações
+                
                 upfile = FileName.decode()
+                # Abri o arquivo em binário para ser lido
                 UploadFile = open("/home/eduardo/Documentos/Projetos/Redes 1/Projeto 2/server//"+upfile,"rb")
                 Read = UploadFile.read(1024)
 
+                # Envia o arquivo lido 
                 while Read:
                     c.send(Read) #Envia 1KB 
                     Read = UploadFile.read(1024)
+
+                print("Arquivo baixado")
+                # Fecha as conexões
                 UploadFile.close()
                 c.close()
 
         elif (data.decode() == "upload"):
+            # Recebe o nome do arquivo
             FileName = c.recv(1024)
             downfile = FileName.decode()
+
             Data = c.recv(1024)
+            # Abrindo o arquivo que vai ser baixado
             DownloadFile = open(downfile,"wb")
-            
+            # Loop para "escrever" o arquivo selecionado
             while Data:
                 DownloadFile.write(Data)
                 Data = c.recv(1024)
